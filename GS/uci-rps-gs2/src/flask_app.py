b6dc6d1e8c2cae2424dad81bd79035e412d8ddce
alexy-ok <alexanderyin5280@gmail.com>
2025-01-24 15:34:27 -0800

    working acceleration backend

--- GS/uci-rps-gs2/src/flask_app.py
+++ GS/uci-rps-gs2/src/flask_app.py
@@ -2,41 +2,77 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 import serial
 import threading
-
+import sys
+from collections import deque, defaultdict
+import datetime
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "http://localhost:3000", "supports_credentials": True}})
 
-sensor_data = ''
-
-def read_serial():
+sensor_data = {}
+sensor_data_lock = threading.Lock()
+def read_serial( port='COM5', baudrate=57600):
     global sensor_data
-    ser = serial.Serial('COM4', 57600) # for Windows
-    # ser = serial.Serial('/dev/tty.usbserial-B000J0YT', 57600) # for Mac
-    while True:
-        line = ser.readline().strip().decode()
-        # print(line)
-        # print(type(line))
-        data_list = line.split(',')
-        # print(data_list)
-        if len(data_list) == 15:
-            try:
-                sensor_data = line
-            except ValueError:
-                print("Invalid sensor data format")
+    global data
+    print('Reading serial data...')
+    try: 
+        ser = serial.Serial(port, baudrate) # for Windows
+        # ser = serial.Serial('/dev/tty.usbserial-B000J0YT', 57600) # for Mac
+    except serial.SerialException as e:
+        print(f"Error: {e}")
+        sys.exit(1)
 
-serial_thread = threading.Thread(target=read_serial)
-serial_thread.daemon = True
-serial_thread.start()
+    try:
+        while True:
+    
+            line = ser.readline().decode("utf-8").strip()
+            # print(line)
+            # print(type(line))
+            data_list = line.split(',')
+            # print(data_list)
+            if len(data_list) == 3:
+                with sensor_data_lock:
+                    sensor_data['acceleration'] = (data_list[0], data_list[1], data_list[2]) 
+                    sensor_data['timestamp'] = datetime.datetime.now()#.isoformat()
+
+            print(sensor_data['acceleration'])
+            print(sensor_data['timestamp'])
+    except ValueError:
+        print("Invalid sensor data format: ValueError")
+    except serial.SerialException as e:
+        print(f"Serial Error: {e}")
+    except:
+        print("Error: Unknown")
+    finally:
+        ser.close()
+
+        
+def start_serial_thread():
+    serial_thread = threading.Thread(target=read_serial, daemon = True )
+
+    serial_thread.start()
+    return serial_thread
 
 @app.route('/data')
 def get_data():
+    """
+    Return the most recently read sensor data in JSON form.
+    If data is not yet available, return an error message with status 503.
+    """
     # print("REQUEST RECEIVED")
-    print(sensor_data)
     # print(jsonify(sensor_data))
     # print("----------------")
-    return jsonify(sensor_data)
+    with sensor_data_lock:
+        if not sensor_data:
+            return jsonify({"error": "No data available"}), 503
+
+        return jsonify({
+            "acceleration": sensor_data['acceleration']
+        })
+
     # return sensor_data
 
 if __name__ == '__main__':
+    start_serial_thread()
     app.run(debug=True)
+    
